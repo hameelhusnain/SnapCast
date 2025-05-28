878f12abc7698a546a06614df4a24a583decd7ee
hameelhusnain <husnainhameel3@gmail.com>
2025-05-28 17:48:19 +0500

    feat: add Header component and integrate it into the main Page

--- snapcast/app/(root)/page.tsx
+++ snapcast/app/(root)/page.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/Header"
+
 const Page = () => {
   return (
      <main className="wrapper page">
+      <Header />
       <h1 className="text-3xl font-karla">Welcome to SnapCast</h1> 
 
      </main>
