397a500715d5f2b20d1ac5ca007cc2b29608bef7
hameelhusnain <husnainhameel3@gmail.com>
2025-05-28 18:07:43 +0500

    feat: enhance Header component with title and subHeader props

--- snapcast/app/(root)/page.tsx
+++ snapcast/app/(root)/page.tsx
@@ -3,9 +3,8 @@ import Header from "@/components/Header"
 const Page = () => {
   return (
      <main className="wrapper page">
-      <Header />
+      <Header title="All videos" subHeader="Public Library"/>
       <h1 className="text-3xl font-karla">Welcome to SnapCast</h1> 
-
      </main>
   )
 }
--- snapcast/components/Header.tsx
+++ snapcast/components/Header.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
 
-const Header = () => {
+const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <section className="header-container">
+        <div className="details">
+          {userImg && (
+            <Image src={userImg || 'assets/images/dummy.jpg'} alt="User Image" width={50} height={50} className="rounded-full" />
+          )}
+
+          <article>
+            <p>{subHeader}</p>
+            <h1>{title}</h1>
+          </article>
+        </div>
+
+        <aside>
+          <Link href="/upload">
+          <Image src="/assets/icons/upload.svg" alt="Upload Icon" width={16} height={16} className="upload-icon" />
+          <span>Uplaod a Video!</span>
+          </Link>
+        </aside>
+
+
+
+      </section>
+    </header>
   )
 }
 
