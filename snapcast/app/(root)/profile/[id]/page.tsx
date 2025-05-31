e1b4dee169cdb5f8ac816fb5b18a3a4101ef5e1a
hameelhusnain <husnainhameel3@gmail.com>
2025-05-31 13:28:01 +0500

    feat: create profile page with Header component integration

---
+++ snapcast/app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+const page = async ({params}: ParamsWithSearch) => {
+    const { id } = await params;
+  return (
+    <div className="wrapper page">
+        <Header subHeader="husnainhameel3@gmail.com" title=" hameelhusnain | HHB" userImg="/assets/images/dummy.jpg" />
+       <h1 className="text-2xl font-karla"> USER ID: {id} </h1>
+        </div>
+  )
+}
+
+export default page
