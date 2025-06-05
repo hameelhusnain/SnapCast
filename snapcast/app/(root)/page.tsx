3856f652c6c231cac62cc07f5eb58d2719d57bc8
hameelhusnain <husnainhameel3@gmail.com>
2025-06-06 01:30:35 +0500

    fix: integrate dummyCards into Page component and define Visibility type

--- snapcast/app/(root)/page.tsx
+++ snapcast/app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import Header from "@/components/Header"
 import VideoCard from "@/components/VideoCard"
+import { dummyCards } from "@/constants"
 const Page = () => {
   return (
      <main className="wrapper page">
       <Header title="All videos" subHeader="Public Library"/>
       <h1 className="text-3xl font-karla">Welcome to SnapCast</h1>
-      <VideoCard 
+
+      {dummyCards.map((card) => (
+        <VideoCard {...card} visibility={card.visibility as Visibility} />
+      ))}
+            <VideoCard 
       id="1"
       title="SnapChat Message"
       thumbnail="/assets/samples/thumbnail (1).png"
