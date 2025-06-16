ffdf89c13bd2d8b7981632bc8ba39a3640a5b29c
hameelhusnain <husnainhameel3@gmail.com>
2025-06-16 23:25:58 +0500

    feat: enhance middleware to handle session validation and redirect to sign-in

--- snapcast/middleware.ts
+++ snapcast/middleware.ts
@@ -6,4 +6,14 @@ export async function middleware(request: NextRequest, response: NextResponse){
     const session = await auth.api.getSession({
         headers: await headers(), 
     })
+
+    if (!session) {
+        return NextResponse.redirect(new URL("/auth/sign-in", request.url))
+    }
+    return NextResponse.next();
 }
+export const config = {
+    matcher: [
+        "/((?!api|_next/static|_next/image|favicon.ico|auth).*)",
+        ],
+    }
