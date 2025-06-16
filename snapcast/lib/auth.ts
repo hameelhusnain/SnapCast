4306044a4e047e6f71374b43ab38641395276da3
hameelhusnain <husnainhameel3@gmail.com>
2025-06-16 23:01:07 +0500

    feat: integrate authentication schema into drizzle adapter and add auth-client module

--- snapcast/lib/auth.ts
+++ snapcast/lib/auth.ts
@@ -2,7 +2,20 @@ import { db } from "@/drizzle/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import pg from 'pg';
+import { schema } from "@/drizzle/schema";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, { provider: "pg" })
+    database: drizzleAdapter(db, { 
+        provider: "pg", 
+        schema,
+    }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }
+    },
+    plugins: [nextCookies()],
+    baseURL: process.env.NEXTAUTH_URL!,
 })
