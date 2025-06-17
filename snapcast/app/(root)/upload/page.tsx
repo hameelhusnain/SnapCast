70c9e0e4de72f9deb0fcf665a08410a5dc8076da
hameelhusnain <husnainhameel3@gmail.com>
2025-06-17 18:29:30 +0500

    feat: enhance upload page with error handling and improved layout

--- snapcast/app/(root)/upload/page.tsx
+++ snapcast/app/(root)/upload/page.tsx
@@ -1,12 +1,23 @@
+'use client';
 import FileInput from "@/components/FileInput"
 import FormField from "@/components/FormField"
+import { useState } from "react"
 
 
 const page = () => {
+    const[error, setError] = useState(null);
   return (
-    <div className="wrapper page">
-        <FormField />
-        <FileInput />
+    <div className="wrapper-md upload-page">
+        <h1>Upload a Video!</h1>
+
+        {error && <div className="error">{error}</div>}
+
+        <form className="rounded-20 shadow-10 gap-6 w-full flex flex-cl px-5 py-7.5">
+            <FormField />
+            <FileInput />
+            <button className="btn btn-primary">Upload</button>
+        </form>
+
     </div>
   )
 }
