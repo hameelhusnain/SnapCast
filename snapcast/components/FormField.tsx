fff8366636a3c8180869003e3e9435142f1197b3
hameelhusnain <husnainhameel3@gmail.com>
2025-06-19 23:07:11 +0500

    feat: add props to FormField component for dynamic rendering

--- snapcast/components/FormField.tsx
+++ snapcast/components/FormField.tsx
@@ -1,4 +1,6 @@
-const FormField = () => {
+import { Placeholder } from "drizzle-orm"
+
+const FormField = ({ id, label, type-'text', value, onChange, Placeholder, as = 'input'}) => {
   return (
     <div>FormField</div>
   )
