3c44017ec83159c4a626160f68d6dfb71dece639
hameelhusnain <husnainhameel3@gmail.com>
2025-06-19 23:10:27 +0500

    feat: add option prop to FormField component for enhanced flexibility

--- snapcast/components/FormField.tsx
+++ snapcast/components/FormField.tsx
@@ -1,8 +1,10 @@
 import { Placeholder } from "drizzle-orm"
 
-const FormField = ({ id, label, type-'text', value, onChange, Placeholder, as = 'input'}) => {
+const FormField = ({ id, label, type-'text', value, onChange, Placeholder, as = 'input', option=[]}: FormFieldProps) => {
   return (
-    <div>FormField</div>
+    <div className="form-field">
+      <label htmlFor={id}>{lable}</label>
+    </div>
   )
 }
  
