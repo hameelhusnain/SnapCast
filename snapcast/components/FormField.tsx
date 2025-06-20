f813a18db46b766ff831d2b574135c5f6c127c23
hameelhusnain <husnainhameel3@gmail.com>
2025-06-20 22:55:16 +0500

    fix: correct syntax and structure in FormField component

--- snapcast/components/FormField.tsx
+++ snapcast/components/FormField.tsx
@@ -1,11 +1,17 @@
 import { Placeholder } from "drizzle-orm"
 
-const FormField = ({ id, label, type-'text', value, onChange, Placeholder, as = 'input', option=[]}: FormFieldProps) => {
-  return (
-    <div className="form-field">
-      <label htmlFor={id}>{lable}</label>
-    </div>
-  )
+const FormField = ({ id, label, type- 'text', value, onChange, Placeholder, as = 'input', option = []}: FormFieldProps) => {
+  const InputToRender = (type) => {
+    if(type === 'textarea') {
+      return <textPath />
+    }
+  }
 }
- 
+return (
+  <div className="form-field">
+    <label htmlFor={id}>{lable}</label>
+  </div>
+)
+}
+
 export default FormField
