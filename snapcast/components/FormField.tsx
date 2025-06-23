e3a01540a39841370491712c5cc6a8a0e88c0761
shabanmanzoor007 <muhammad.shaban24@my.northampton.ac.uk>
2025-06-23 23:56:40 +0500

    fix: remove unused imports and clean up FormField component

--- snapcast/components/FormField.tsx
+++ snapcast/components/FormField.tsx
@@ -1,5 +1,3 @@
-import { Placeholder } from "drizzle-orm";
-import { type } from "os";
 import React from "react";
 
 interface Option {
@@ -78,22 +76,3 @@ const FormField = ({
 };
 
 export default FormField;
-
-const FormField = ({ id, label, type- 'text', value, onchange, Placeholder, as = 'input', Option = []}: FormFieldProps) => {
-  const InputToRender = (type: string) => {
-
-const FormField = ({ id, label, type- 'text', value, onChange, Placeholder, as = 'input', option = []}: FormFieldProps) => {
-  const InputToRender = (type) => {
-    if(type === 'textarea') {
-      return <textPath />
-    }
-  }
-}
-return (
-  <div className="form-field">
-    <label htmlFor={id}>{lable}</label>
-  </div>
-)
-}
-
-export default FormField
