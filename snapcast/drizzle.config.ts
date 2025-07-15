2f63197e43c4f722867e2a334980742a7a0ab1fc
shabanmanzoor007 <muhammad.shaban24@my.northampton.ac.uk>
2025-07-15 11:42:26 +0500

    Refactor database schema and configuration for improved clarity and structure

--- snapcast/drizzle.config.ts
+++ snapcast/drizzle.config.ts
@@ -1,13 +1,12 @@
-import {config} from 'dotenv';
-import {defineConfig} from 'drizzle-kit';
-
-config({ path: './snapcast/.env' });
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+config({ path: "./.env" });
 
 export default defineConfig({
-    schema: './drizzle/schema.ts',
-    out: './drizzle/migrations',
-    dialect: 'postgresql',
-    dbCredentials: {
-            url: process.env.DATABASE_URL_POSTGRES!,
-        }
-    });
+  schema: "./drizzle/schema.ts",
+  out: "./drizzle/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL_POSTGRES!,
+  },
+});
